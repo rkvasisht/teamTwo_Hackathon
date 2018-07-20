01e06b44a90599665cbd05661289e281babc71a4
Mark Sommers <kacysommers@gmail.com>
2018-07-20 10:07:30 -0700

    signup page, accordians

--- static/js/main.js
+++ static/js/main.js
@@ -6,9 +6,4 @@ $(document).ready(function() {
     // shade clicked tags
     $(".tag").on("click", function() {
     $(this).toggleClass("shaded") })
-
-    // jQuery Ajax request
-    // $.get('https://data.seattle.gov/resource/qc9h-bn8y.json').done(function(returnData) {
-    //     console.log(returnData)
-    // });
 });
